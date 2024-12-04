2e47c1a760cfb0a7f2be77d45a3339a88a1104c9
Shrikant Yadav <shrikant9907@gmail.com>
2024-12-04 14:28:45 +0530

    deployment fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/todo-list-next-js-15',
+    assetPrefix: '/todo-list-next-js-15',
+    trailingSlash: true,
 };
 
 export default nextConfig;
